e9e644568fa19169b01a1eb4fe558f52d15d2868
Andres Ozma <ozmafromhell@gmail.com>
2024-10-05 19:16:00 -0300

    cambio de puerto

--- input/app.py
+++ input/app.py
@@ -13,4 +13,4 @@ def get_time():
     return jsonify(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5005)
